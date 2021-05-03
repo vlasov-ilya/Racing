0e6bd62047da27244296cc6d37a5e332e8e51219
Ilya Vlasov <ilyavlasov93@gmail.com>
2021-05-02 22:16:24 -0400

    beta2

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ let road = document.querySelector(".road")
 let score = 0
 let bet = 0
 let margin
+let winner
+let choise
 
 
 function carMove1() {
@@ -25,24 +27,67 @@ function carMove2() {
   }, 100)
 }
 
+document.querySelector('.cars').onclick = function() {
+  return carMove1(), carMove2()
+}
 
+function winerCheck() {
+  if (car1.margin >= 100 + '%'){
+    winner = car1
+  } else if (car2.margin >= 100 + '%'){
+    winner = car2
+  }
+}
 
-function betChange() {
-  if (plus){
-    bet = bet + 1 
-  }else if(minus  && bet > 0){
-    bet = bet - 1
-  } else (
-    alert ("ставка не может быть меньше 0")
-  )
+function betChangePlus() {
+  if (score == 0){
+    bet = 1 
+  } else {
+    bet++
+  }
+
   document.querySelector('.bet').innerHTML = 'Твоя ставка: ' + bet
 }
 
+function betChangeMinus() {
+if(bet > 0){
+  bet--
+} else (
+  alert ("ставка не может быть меньше 0")
+)
+  document.querySelector('.bet').innerHTML = 'Твоя ставка: ' + bet
+}
+
+plus.onclick = function() {
+  return betChangePlus()
+}
+minus.onclick = function() {
+  return betChangeMinus()
+}
+
+// function carChoise() {
+//   car = document.querySelector('.car')
+//   choise = event.target
+//   choise.addEventListener('click', car)
+
+// }
+
+function scoreCount() {
+  if (choise === winner) {
+    alert ("Ставка сыграла! \n Твой выйгрыш составил: " + bet)
+    score = score + bet
+  } else {
+    alert ("Ты проиграл!")
+    if (score > bet){
+    score = score - bet
+    } else {
+      score = 0
+    }
+  }
+}
+
+
 document.querySelector('.score').innerHTML = 'Твой счет: ' + score
 
-document.querySelector('.cars').onclick = function() {
-  return carMove1(), carMove2()}
 
-document.querySelector('button').onclick = function() {
-  return betChange()
-}
+
